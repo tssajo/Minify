e80cf032d3909a6f173d0c88be5b34a650857f7a
Zoltan Frombach <tssajo@gmail.com>
2013-10-03 09:54:13 +0200

    Do not import threading module when sublime.set_timeout_async method method is available. It should speed things up a bit in ST3. Also removed unneeded parentheses from if statements.

--- Minify.py
+++ Minify.py
@@ -1,13 +1,16 @@
-import sublime, sublime_plugin, re, os, subprocess, threading # threading is only needed for ST2
+import sublime, sublime_plugin, re, os, subprocess
 
-if (int(sublime.version()) >= 3000):
+# threading is only needed when sublime.set_timeout_async method is not available (in ST2)
+HAS_ASYNC_TIMEOUT = bool(hasattr(sublime.__class__, 'set_timeout_async') and callable(getattr(sublime.__class__, 'set_timeout_async')))
+if not HAS_ASYNC_TIMEOUT:
+	import threading
+
+if int(sublime.version()) >= 3000:
 	PLUGIN_DIR = os.path.dirname(__file__)
 else:
 	PLUGIN_DIR = os.getcwd()
 
-HAS_ASYNC_TIMEOUT = bool(hasattr(sublime.__class__, 'set_timeout_async') and callable(getattr(sublime.__class__, 'set_timeout_async')))
-
-# Run the shell command in a separate thread in ST2. This is not needed in ST3 because ST3 has the sublime.set_timeout_async method.
+# Run the shell command in a separate thread. This is not needed in ST3 where sublime.set_timeout_async method is available
 class RunCmdInOtherThread(threading.Thread):
 
 	def __init__(self, cmdToRun):
@@ -27,32 +30,32 @@ class MinifyCommand(sublime_plugin.TextCommand):
 		return bool(filename and (len(filename) > 0) and not (re.search('\.(?:css|js)$', filename) is None))
 
 	def handle_thread(self, thread, outfile):
-		if (thread.is_alive()):
+		if thread.is_alive():
 			sublime.set_timeout(lambda: self.handle_thread(thread, outfile), 100)
 		else:
-			if (not thread.result):
+			if not thread.result:
 				sublime.active_window().open_file(outfile)
 
 	def doit(self):
 		inpfile = self.view.file_name()
-		if (inpfile and (len(inpfile) > 0)):
-			if (sublime.load_settings('Minify.sublime-settings').get('save_first') and self.view.is_dirty()):
+		if inpfile and (len(inpfile) > 0):
+			if sublime.load_settings('Minify.sublime-settings').get('save_first') and self.view.is_dirty():
 				self.view.run_command('save')
-			if (not (re.search('\.js$', inpfile) is None)):
+			if not (re.search('\.js$', inpfile) is None):
 				outfile = re.sub(r'(\.js)$', r'.min\1', inpfile, 1)
 				cmd = sublime.load_settings('Minify.sublime-settings').get('uglifyjs_command') or 'uglifyjs';
 				cmdToRun = [cmd, inpfile, '-c', '-m', '-o', outfile]
-			elif (not (re.search('\.css$', inpfile) is None)):
+			elif not (re.search('\.css$', inpfile) is None):
 				outfile = re.sub(r'(\.css)$', r'.min\1', inpfile, 1)
 				cmd = sublime.load_settings('Minify.sublime-settings').get('java_command') or 'java';
 				cmdToRun = [cmd, '-jar', PLUGIN_DIR + '/bin/yuicompressor-2.4.7.jar', inpfile, '-o', outfile, '--charset', 'utf-8', '--line-break', '250']
 			else:
 				cmdToRun = False
-			if (cmdToRun):
+			if cmdToRun:
 				print('Minifying file ' + str(inpfile))
-				if (HAS_ASYNC_TIMEOUT):
+				if HAS_ASYNC_TIMEOUT:
 					result = subprocess.call(cmdToRun, shell=True)
-					if (not result):
+					if not result:
 						sublime.active_window().open_file(outfile)
 				else:
 					thread = RunCmdInOtherThread(cmdToRun)
@@ -60,7 +63,7 @@ class MinifyCommand(sublime_plugin.TextCommand):
 					sublime.set_timeout(lambda: self.handle_thread(thread, outfile), 100)
 
 	def run(self, edit):
-		if (HAS_ASYNC_TIMEOUT):
+		if HAS_ASYNC_TIMEOUT:
 			sublime.set_timeout_async(lambda: self.doit(), 0)
 		else:
 			self.doit()
@@ -74,25 +77,25 @@ class BeautifyCommand(sublime_plugin.TextCommand):
 		return bool(filename and (len(filename) > 0) and not (re.search('\.js$', filename) is None))
 
 	def handle_thread(self, thread, outfile):
-		if (thread.is_alive()):
+		if thread.is_alive():
 			sublime.set_timeout(lambda: self.handle_thread(thread, outfile), 100)
 		else:
-			if (not thread.result):
+			if not thread.result:
 				sublime.active_window().open_file(outfile)
 
 	def doit(self):
 		inpfile = self.view.file_name()
-		if (inpfile and (len(inpfile) > 0)):
-			if (sublime.load_settings('Minify.sublime-settings').get('save_first') and self.view.is_dirty()):
+		if inpfile and (len(inpfile) > 0):
+			if sublime.load_settings('Minify.sublime-settings').get('save_first') and self.view.is_dirty():
 				self.view.run_command('save')
-			if (not (re.search('\.js$', inpfile) is None)):
+			if not (re.search('\.js$', inpfile) is None):
 				outfile = re.sub(r'(?:\.min)?(\.js)$', r'.beautified\1', inpfile, 1)
 				cmd = sublime.load_settings('Minify.sublime-settings').get('uglifyjs_command') or 'uglifyjs';
 				cmdToRun = [cmd, inpfile, '-b', '-o', outfile]
 				print('Beautifying file ' + str(inpfile))
-				if (HAS_ASYNC_TIMEOUT):
+				if HAS_ASYNC_TIMEOUT:
 					result = subprocess.call(cmdToRun, shell=True)
-					if (not result):
+					if not result:
 						sublime.active_window().open_file(outfile)
 				else:
 					thread = RunCmdInOtherThread(cmdToRun)
@@ -100,7 +103,7 @@ class BeautifyCommand(sublime_plugin.TextCommand):
 					sublime.set_timeout(lambda: self.handle_thread(thread, outfile), 100)
 
 	def run(self, edit):
-		if (HAS_ASYNC_TIMEOUT):
+		if HAS_ASYNC_TIMEOUT:
 			sublime.set_timeout_async(lambda: self.doit(), 0)
 		else:
 			self.doit()
